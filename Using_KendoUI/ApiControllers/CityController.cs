f07a7149e5975309e3968113bf14f6e4fc0fb932
aminsh <aminsheikhi@gmail.com>
2014-10-13 15:25:55 +0330

    First sample Tree with web api service

---
+++ Using_KendoUI/ApiControllers/CityController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Using_KendoUI.ApiControllers
+{
+    public class CityController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<City> Cities()
+        {
+            var id = Request.RequestUri.ParseQueryString()["id"] == null
+                ? 0
+                : Convert.ToInt32(Request.RequestUri.ParseQueryString()["id"]);
+
+            if (id == 0)
+                return _cities.Select(c => new City
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    HasChildren = c.HasChildren
+                });
+            var value = _cities.First(c => c.Id == id).Cities.Select(c => new City
+            {
+                Id = c.Id,
+                Name = c.Name,
+                HasChildren = c.HasChildren
+            });
+            return value;
+        }
+
+        private readonly IEnumerable<City> _cities;
+
+        public CityController()
+        {
+            _cities = new List<City>
+            {
+                new City {Id = 1, Name = "Tehran", HasChildren = true , Cities = new List<City>
+                {
+                   new City {Id = 11, Name = "Tajrish", HasChildren = false},
+                   new City {Id = 12, Name = "Lavasan", HasChildren = false},
+                   new City {Id = 13, Name = "Rey", HasChildren = false},
+                }},
+                new City {Id = 2, Name = "Esfahan", HasChildren = true},
+                new City {Id = 3, Name = "Tabriz", HasChildren = true},
+                new City {Id = 4, Name = "Shiraz", HasChildren = true}
+            };
+        }
+    }
+
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool HasChildren { get; set; }
+        public IEnumerable<City> Cities { get; set; }
+    }
+}
---
+++ Using_KendoUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Using_KendoUI.Global" Language="C#" %>
